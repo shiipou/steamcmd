37a051b3bb7164fc1473fd598c6d0e4f5be5fe46
Flavien CADET <flavien.cadet@nocturlab.fr>
2020-04-20 11:30:40 +0200

    Allow to use beta key

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,19 +6,26 @@ if [ -n "${INSTALL_APPS}" ]; then
   for app in ${INSTALL_APPS}; do
     steamcmd_binary="steamcmd"
     password_bypass=""
+    beta_cmd=""
+    if [ -n "${BETA_NAME}" ]; then
+      beta_cmd=" -beta BETA_NAME"
+      if [ -n "${BETA_KEY}" ]; then
+        beta_cmd="${beta_cmd} -betapassword ${BETA_KEY}"
+      fi
+    fi
     if [ -n "${PASSWORD}" ]; then
       password_bypass="echo '${PASSWORD}' | "
     fi
     if [ "${FORCE_INSTALL}" = "true" ]; then
       echo "Force re-install $app"
-      /bin/sh -c "${password_bypass}${steamcmd_binary} +login '${LOGIN}' +force_install_dir /steam/${app} +app_update ${app} validate +quit"
+      /bin/sh -c "${password_bypass}${steamcmd_binary} +login '${LOGIN}' +force_install_dir /steam/${app} +app_update ${app}${beta_cmd} validate +quit"
     else
       echo "Check if app ${app} was installed"
       if [ -d "/steam/${app}" ]; then
         echo "App ${app} already installed"
       else
         echo "App ${app} need to be downloaded"
-        /bin/sh -c "${password_bypass}${steamcmd_binary} +login '${LOGIN}' +force_install_dir /steam/${app} +app_update ${app} validate +quit"
+        /bin/sh -c "${password_bypass}${steamcmd_binary} +login '${LOGIN}' +force_install_dir /steam/${app} +app_update ${app}${beta_cmd} validate +quit"
       fi
     fi
   done
