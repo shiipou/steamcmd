c4027a4ed3132a9465670ff88608a15118991243
Flavien CADET <flavien.cadet@nocturlab.fr>
2020-04-20 12:37:44 +0200

    Create addons_loader.lua

---
+++ examples/gmod/addons_loader.lua
@@ -0,0 +1,2 @@
+resource.AddWorkshop( "160250458" ) -- Wiremod https://steamcommunity.com/sharedfiles/filedetails/?id=160250458
+resource.AddWorkshop( "773402917" ) -- Advanced Duplicator 2 https://steamcommunity.com/sharedfiles/filedetails/?id=773402917
